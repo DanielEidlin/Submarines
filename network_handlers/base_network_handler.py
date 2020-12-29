945f8ad734e6b5e09e899ab07bd0d17ecbd3cd55
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-12-29 16:51:08 +0200

    Separate network handlers to different files.

---
+++ network_handlers/base_network_handler.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from consts import DEFAULT_LISTEN_QUEUE, DEFAULT_BUFFER_SIZE
+
+
+class BaseNetworkHandler(ABC):
+    """
+    Class representing an abstract network client.
+    """
+
+    @abstractmethod
+    def listen(self, bind_ip: str = '', listen_queue: int = DEFAULT_LISTEN_QUEUE):
+        """
+        Listens for an incoming connection.
+        :param bind_ip: IP address to bind to.
+        :param listen_queue: Number of incoming connections to hold in queue.
+        """
+        pass
+
+    @abstractmethod
+    def connect(self, ip: str):
+        """
+        Connects to another network client.
+        :param ip: IP address to connect to.
+        """
+        pass
+
+    @abstractmethod
+    def send(self, data: bytes):
+        """
+        Sends data to network.
+        :param data: Data to send.
+        """
+        pass
+
+    @abstractmethod
+    def receive(self, buffer_size: int = DEFAULT_BUFFER_SIZE) -> bytes:
+        """
+        Receives data from network.
+        :param buffer_size: Size of data to receive.
+        :return: Received data.
+        """
+        pass
--- network_handlers/tcp_network_handler.py
+++ network_handlers/tcp_network_handler.py
@@ -1,49 +1,6 @@
-from abc import ABC, abstractmethod
 from socket import socket, AF_INET, SOCK_STREAM
-
-DEFAULT_LISTEN_QUEUE = 5
-PORT = 3000
-DEFAULT_BUFFER_SIZE = 1024
-
-
-class BaseNetworkHandler(ABC):
-    """
-    Class representing an abstract network client.
-    """
-
-    @abstractmethod
-    def listen(self, bind_ip: str = '', listen_queue: int = DEFAULT_LISTEN_QUEUE):
-        """
-        Listens for an incoming connection.
-        :param bind_ip: IP address to bind to.
-        :param listen_queue: Number of incoming connections to hold in queue.
-        """
-        pass
-
-    @abstractmethod
-    def connect(self, ip: str):
-        """
-        Connects to another network client.
-        :param ip: IP address to connect to.
-        """
-        pass
-
-    @abstractmethod
-    def send(self, data: bytes):
-        """
-        Sends data to network.
-        :param data: Data to send.
-        """
-        pass
-
-    @abstractmethod
-    def receive(self, buffer_size: int = DEFAULT_BUFFER_SIZE) -> bytes:
-        """
-        Receives data from network.
-        :param buffer_size: Size of data to receive.
-        :return: Received data.
-        """
-        pass
+from base_network_handler import BaseNetworkHandler
+from consts import DEFAULT_LISTEN_QUEUE, PORT, DEFAULT_BUFFER_SIZE
 
 
 class TCPNetworkHandler(BaseNetworkHandler):
