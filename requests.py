94898a812707ecda9cf132baf779d4ee77fc352a
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-12-29 14:12:02 +0200

    Add ErrorRequest class.

--- requests.py
+++ requests.py
@@ -63,14 +63,37 @@ class AnswerRequest(BaseRequest):
     Class representing an ANSWER request.
     """
 
-    def __init__(self, x_coordinate: int, y_coordinate: int, status: Status):
+    def __init__(self, status: AnswerStatus, x_coordinate: int = None, y_coordinate: int = None):
         """
         Default constructor.
+        :param status: Answer request status.
         :param x_coordinate: X coordinate of the attempt.
         :param y_coordinate: Y coordinate of the attempt.
         """
+        self.status = status
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
+
+    def to_dict(self) -> dict:
+        """
+        Returns the request as a dictionary.
+        :return: The request as dictionary.
+        """
+        if self.x_coordinate and self.y_coordinate:
+            return dict(REQUIRED_FIELDS, **{"TYPE": "ANSWER"},
+                        **{"X-COOR": self.x_coordinate, "Y-COOR": self.y_coordinate}, **{"STATUS": self.status.name})
+        return dict(REQUIRED_FIELDS, **{"TYPE": "ANSWER"}, **{"STATUS": self.status.name})
+
+
+class ErrorRequest(BaseRequest):
+    """
+    Class representing an ERROR request.
+    """
+    def __init__(self, status: ErrorStatus):
+        """
+        Default constructor.
+        :param status: Error request status.
+        """
         self.status = status
 
     def to_dict(self) -> dict:
@@ -78,5 +101,5 @@ class AnswerRequest(BaseRequest):
         Returns the request as a dictionary.
         :return: The request as dictionary.
         """
-        return dict(REQUIRED_FIELDS, **{"TYPE": "ANSWER"},
-                    **{"X-COOR": self.x_coordinate, "Y-COOR": self.y_coordinate}, **{"STATUS": self.status.name})
+        return dict(REQUIRED_FIELDS, **{"TYPE": "ERROR"}, **{"STATUS": self.status.name})
+
