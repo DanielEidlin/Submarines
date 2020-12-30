cb3fb83db8181ae98fbf053133f790d0d0ece0e6
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-12-30 10:44:53 +0200

    Create Exceptions.

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class OutOfRangeException(Exception):
+    pass
+
+
+class AttemptNotInTurnException(Exception):
+    pass
+
+
+class ClosedException(Exception):
+    pass
+
+
+class UnexpectedException(Exception):
+    pass
--- validators/ready_validator.py
+++ validators/ready_validator.py
@@ -1,11 +1,11 @@
-from base_validator import BaseValidator
 from parsers.base_parser import BaseParser
 from requests.error_request import ErrorRequest
 from statuses.error_statuses import ErrorStatus
+from exceptions import UnexpectedException, ClosedException
 from network_handlers.base_network_handler import BaseNetworkHandler
 
 
-class ReadyValidator(BaseValidator):
+class ReadyValidator:
     def __init__(self, request: dict, network_handler: BaseNetworkHandler, parser: BaseParser):
         self.request = request
         self.network_handler = network_handler
@@ -14,17 +14,17 @@ class ReadyValidator(BaseValidator):
     def validate_request_type(self):
         request_type = self.request.get("TYPE", None)
         if not request_type:
-            raise UnexcpectedException
+            raise UnexpectedException
         if request_type == "ERROR" and self.request.get("STATUS") == "CLOSED":
             raise ClosedException
         if request_type != "READY":
-            raise UnexcpectedException
+            raise UnexpectedException
 
     def validate(self):
         try:
             self.validate_request_type()
         except ClosedException:
             raise
-        except UnexcpectedException:
+        except UnexpectedException:
             data = self.parser.pack(ErrorRequest(ErrorStatus.UNEXPECTED).to_dict())
             self.network_handler.send(data)
